44d348e0364051a8f475aae15cdc9d16f4d2e84a
isaac-ward <isaacronaldward@gmail.com>
2024-06-05 17:42:49 -0700

    Committing changes pre mark merge. MPPI benchmarking in place

--- src/globals.py
+++ src/globals.py
@@ -11,9 +11,9 @@ REACHED_SAMPLE_REGION_THRESHOLD = 0.5 # m
 REACHED_ENTRY_POINT_THRESHOLD   = 0.25 # m
 
 # For testing disturbances
-DISTURBANCE_VARIANCE_ROTORS = MAX_THRUST_PER_PROP * 0.33
-DISTURBANCE_VELOCITY_VARIANCE_WIND = 0.5 # m/s
-DISTURBANCE_ANGULAR_VELOCITY_VARIANCE_WIND = 0.5 # rad/s
+DISTURBANCE_VARIANCE_ROTORS = MAX_THRUST_PER_PROP * 0.0
+DISTURBANCE_VELOCITY_VARIANCE_WIND = 0.0 # m/s
+DISTURBANCE_ANGULAR_VELOCITY_VARIANCE_WIND = 0.0 # rad/s
 
 # TODO should define goal states (i.e. set velocities and angles)
 MAP_CONFIGS = {
@@ -22,32 +22,37 @@ MAP_CONFIGS = {
         "metres_per_pixel": METRES_PER_PIXEL,
         "start_coord_metres": (1.5, 2),   # x, y
         "finish_coord_metres": (6.5, 2),
+        "fudge_factor": 1.6,
     },
     "3x28": {
         "filename": "3x28.png",
         "metres_per_pixel": METRES_PER_PIXEL,
         "start_coord_metres": (1.5, 2),
         "finish_coord_metres": (27.5, 2),
+        "fudge_factor": 1.6,
     },
     "downup": {
         "filename": "downup.png",
         "metres_per_pixel": METRES_PER_PIXEL,
         "start_coord_metres": (1.5, 8),
         "finish_coord_metres": (27.5, 8),
+        "fudge_factor": 1.6,
     },
     "downup-o": {
         "filename": "downup-o.png",
         "metres_per_pixel": METRES_PER_PIXEL,
         "start_coord_metres": (1.5, 8),
         "finish_coord_metres": (27.5, 8),
+        "fudge_factor": 1.6,
     },
     "grapevine": {
-        "filename": "grapevine.png",
+        "filename": "grapevine2x.png",
         "metres_per_pixel": METRES_PER_PIXEL,
         # If you have a map with pixels, this is how you can do the start end points,
         # note that x value is right from the left of the image, and the y value is 
         # up from the bottom of the image
-        "start_coord_metres": 2 * np.array((300, 3000 - 300)) * METRES_PER_PIXEL,
-        "finish_coord_metres": 2 * np.array((1500, 3000 - 2100)) * METRES_PER_PIXEL
+        "start_coord_metres": 2*np.array((300, 3000 - 300)) * METRES_PER_PIXEL,
+        "finish_coord_metres": 2*np.array((1500, 3000 - 2100)) * METRES_PER_PIXEL,
+        "fudge_factor": 2.6,
     },
 }
--- src/main_mppi.py
+++ src/main_mppi.py
@@ -2,6 +2,7 @@ import numpy as np
 from tqdm import tqdm
 import os
 import pickle
+import time 
 
 from map import Map 
 import path
@@ -19,14 +20,14 @@ if __name__ == "__main__":
     log_folder = utils.make_log_folder(name="run")
 
     # Get the map info for the map that we'll be using
-    map_config = globals.MAP_CONFIGS["grapevine"]
+    map_config = globals.MAP_CONFIGS["downup-o"]
     metres_per_pixel    = map_config["metres_per_pixel"]
     filename            = map_config["filename"]
     start_coord_metres  = map_config["start_coord_metres"]
     finish_coord_metres = map_config["finish_coord_metres"]
+    fudge_factor        = map_config["fudge_factor"]
 
     # Other configuration variables which we will use later
-    fudge_factor = 1.6
     nominal_downsample_factor = 0.05
     fitted_downsample_factor = 0.1
     map_scale_factor = 0.2
@@ -79,6 +80,16 @@ if __name__ == "__main__":
         )
         nominal_xy = nominal_xy.downsample_to_average_adjacent_distance_metres(nominal_downsample_factor)
 
+        # Compute the boxes along the path for the purpose of bezier curve fitting
+        boxes = map.path_box(nominal_xy)
+
+        # Smooth it with respect to the boundaries by first getting 
+        # obstacle/freespace boxes
+        state_vector, control_vector = dyn.bezier_trajectory_fitting(nominal_xy.path_metres, boxes)
+        fitted_path_metres = state_vector[:,[0,2]]
+        fitted_nominal_xy = path.Path(fitted_path_metres)
+        fitted_nominal_xy = fitted_nominal_xy.downsample_to_average_adjacent_distance_metres(fitted_downsample_factor)
+
         visuals_legacy.vis_occupancy_grid(
             filepath=f"{log_folder}/occupancy_grid.png",
             occupancy_grid=map.occupancy_grid,
@@ -89,18 +100,10 @@ if __name__ == "__main__":
                 finish_coord_metres
             ],
             path_metres=nominal_xy.path_metres,
-            #path2_metres=fitted_nominal_xy.path_metres,
+            path2_metres=fitted_nominal_xy.path_metres,
             plot_coordinates=True,
-            #path_boxes=boxes
+            path_boxes=boxes
         )
-
-        # Smooth it with respect to the boundaries by first getting 
-        # obstacle/freespace boxes
-        boxes = map.path_box(nominal_xy)
-        state_vector, control_vector = dyn.bezier_trajectory_fitting(nominal_xy.path_metres, boxes)
-        fitted_path_metres = state_vector[:,[0,2]]
-        fitted_nominal_xy = path.Path(fitted_path_metres)
-        fitted_nominal_xy = fitted_nominal_xy.downsample_to_average_adjacent_distance_metres(fitted_downsample_factor)
 
         # Save to cache
         cache["nominal_xy"] = nominal_xy
@@ -110,12 +113,16 @@ if __name__ == "__main__":
             pickle.dump(cache, f)
 
     # --------------------------------
+            
+    # Start timing the experiment
+    start_time = time.time()
 
     # Create an MPPI controller
     controller = MPPI(
         dynamics_fn=dyn.dynamics_true_no_disturbances,
         # Allow reverse thrust
         control_bounds_lower=np.array([-globals.MAX_THRUST_PER_PROP, -globals.MAX_THRUST_PER_PROP]),
+        #control_bounds_lower=np.array([0, 0]),
         control_bounds_upper=np.array([+globals.MAX_THRUST_PER_PROP, +globals.MAX_THRUST_PER_PROP]),
         K=256,
         H=20,
@@ -124,6 +131,8 @@ if __name__ == "__main__":
         map=map
     )
     print(f"Created MPPI controller with {controller.K} samples and horizon {controller.H} ({controller.H * dt} seconds lookahead)")
+
+    disturbances_on = False
 
     # Enter a loop where we apply the controller, get a control, roll forward
     # the dynamics and repeat
@@ -153,13 +162,11 @@ if __name__ == "__main__":
         scored_rollouts_per_step.append((samples, scores))
         u = U[0]
 
-        # There could be a disturbance in the control (e.g. motor noise, wind)
-        # motor noise is modeled by drawing from a gaussian with set variance
-        u_disturbance_rotors = np.random.normal(0, globals.DISTURBANCE_VARIANCE_ROTORS**0.5, size=dyn.m_dim) 
-        u += u_disturbance_rotors
-
         # Roll forward the dynamics
-        x = dyn.dynamics_true_no_disturbances(x, u)
+        if disturbances_on:
+            x = dyn.dynamics_true(x, u)
+        else:
+            x = dyn.dynamics_true_no_disturbances(x, u)
 
         # The game is over if we hit a wall, or if we go out of bounds
         if map.does_point_hit_boundary(x[0], x[2]) or map.out_of_bounds(x[0], x[2]):
@@ -196,6 +203,21 @@ if __name__ == "__main__":
             "v": f"{np.linalg.norm(x[[1,3]]):.2f}",
         })
         pbar.update(1)        
+
+    # Stop timing
+    end_time = time.time()
+    print(f"Experiment took {end_time - start_time:.4f} clock seconds")
+    print(f"Experiment took {len(state_trajectory) * dt:.4f} simulated seconds")
+
+    # Report the final analysis
+    visuals.state_trajectory_analysis(
+        f"{log_folder}/state_trajectory_analysis.png",
+        state_trajectory,
+    )
+    visuals.control_trajectory_analysis(
+        f"{log_folder}/control_trajectory_analysis.png",
+        control_trajectory,
+    )
         
     # --------------------------------
 
@@ -216,7 +238,7 @@ if __name__ == "__main__":
                 "color": "grey",
             }
         ], 
-        simulation_dt=dt,
+        simulation_dts=[dt for _ in range(len(state_trajectory))],
         state_trajectory=state_trajectory,
         control_trajectory=control_trajectory,
         scored_rollouts_per_step=scored_rollouts_per_step,
