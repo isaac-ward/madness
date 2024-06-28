f16d1286209ce7a522ce98a2ece43361e838d7a8
isaac-ward <isaacronaldward@gmail.com>
2024-06-27 21:37:59 -0700

    Working on the map, rejigging for user specified voxel inputs

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# Disturbance-Resilient Optimal Control of Quadrotors for Cave Exploration and Sample Retrieval Missions
-
-![Model Predictive Path Integral Control (MPPI) uses random sampling and a hand-defined cost function to determine the optimal cost-weighted control. This can result in interesting and dynamic behavior, depending on the properties of the cost function.](https://github.com/isaac-ward/madness/blob/main/assets/_demonstration.gif)
-
-This work investigates and compares multiple optimal control methods in the context of a cave exploration and sample retrieval mission. We use state of the art path planning techniques in tandem with iterative linear quadratic regulator (iLQR) and model predictive path integral control (MPPI) control to demonstrate that a quadrotor can be optimally controlled 1) within a complex environment and 2) in the presence of motor and wind-based model disturbances. We provide quantitative results benchmarking the performance and resilience of these control schemes in the face of realistic environmental and actuator disturbances.
+# madness
 
 ## Running this
 
@@ -20,6 +16,7 @@ conda env export --no-builds | grep -v "^prefix: " > env.yaml
 
 Can then run any of the main files like so:
 ```
+source C:/Users/moose/miniconda3/etc/profile.d/conda.sh
 conda activate madness
 python src/main.py
 ```
@@ -39,50 +36,6 @@ Alternatively, you can use pip:
 pip install scipy numpy matplotlib tqdm networkx moviepy fastdtw cvxpy
 ```
 
-## Initial 2D Dynamics
-
-We will be modeling our manipulator-arm drone with quadrotor dynamics sourced from AA274A and AA203.
-
-<img src="readme_assets/planar_quadrotor.png" height="300"/>
-
-The dynamic model for this quadrotor is given by
-
-<img src="readme_assets/dynamics.png" height="200"/>
-
-Where 
-* 	(x,y) is the position 
-*	(v_x,v_y) is the translational velocity
-*	ϕ is the pitch of the quadrotor
-*	ω is the pitching rate of the quadrotor
-*	(T_1,T_2) is the thrust from the rotors 
-*	C_D^v is the translational drag
-*	C_D^ϕ is the rotational drag
-*	m is the mass
-*	I_yy is the moment of inertia
-*	l is half the length of the quadrotor
-*	(w_x,w_y) is the wind velocity
-*	g is the gravitational acceleration
-
-We will define our state and control vectors as
-
-<img src="readme_assets/state_and_control.png" height="100"/>
-
-First and foremost, our drone will operate in discrete time. Because of this, we need to convert our continuous time model to a discrete model. This can be accomplished with Euler discretized dynamics which will give us
-
-<img src="readme_assets/discrete.png" height="200"/>
-
-We will be given a trajectory we wish to follow with this quadrotor. To accomplish this, we need a controller. Let’s use an optimal closed loop controller, say LQR. LQR is a great controller for linear tracking problems, but oh no! Our dynamics are nonlinear!
-
-To use an LQR controller, we will need to linearize the dynamics. We can accomplish this by linearizing about the nominal trajectory. This will give us the model
-
-<img src="readme_assets/linearization.png" height="200"/>
-
-Finally, we can pose the cost function for the LQR problem as
-
-<img src="readme_assets/cost.png" height="75"/>
-
-Since we are solving this over a trajectory of points, we will need to use iLQR techniques to solve the control problem.
-
 ## Links
 
 For 3D visualization:
@@ -98,9 +51,4 @@ For 3D visualization:
 To remove all logs (minus the gitkeep file)
 ```
 rm -rf logs/*
-```
-
-Benchmarking all methods
-```
-python src/main_ilqr.py && python src/main_mppi.py && python src/main_ol.py
 ```
--- src/map.py
+++ src/map.py
@@ -1,1 +1,163 @@
-# stores 3d map data
+import math
+import numpy as np 
+from PIL import Image
+import scipy
+import scipy.ndimage
+import scipy.spatial    
+import networkx as nx
+from tqdm import tqdm
+import copy
+
+def test_points():
+    # It's gonna be N points from 0,0,0 to 10,0,0
+    N = 100
+    points = np.zeros((N,3))
+    points[:,0] = np.linspace(0,10,N)
+    return points
+
+class Map:
+    def __init__(
+        self,
+        map_filepath,
+        voxel_per_x_metres,
+        extents_metres
+    ):
+        """
+        Input filepath is where we can find the map file. Maps are represented with
+        point clouds, where each point is a (x,y,z) in metres
+
+        We will create a voxel grid representation of the map, where each voxel is
+        voxel_per_x_metres on a side, and a cube
+        """
+        
+        # Load the occupancy grid 
+        self.map_filepath = map_filepath
+        self.voxel_per_x_metres = voxel_per_x_metres
+        self.extents_metres = extents_metres
+        
+        # Load the map file as an occupancy grid
+        self.points = test_points()
+
+        # What are the extents of the point cloud?
+        def min_max_in_axis(points, axis):
+            # Return the mid max, ensure they are separated by at least 
+            # voxel_per_x_metres
+            min_val = np.min(points[:,axis])
+            max_val = np.max(points[:,axis])
+            difference = max_val - min_val
+            if difference < self.voxel_per_x_metres:
+                min_val -= (self.voxel_per_x_metres - difference) / 2
+                max_val += (self.voxel_per_x_metres - difference) / 2
+            return min_val, max_val
+        extents_x = min_max_in_axis(self.points, 0)
+        extents_y = min_max_in_axis(self.points, 1)
+        extents_z = min_max_in_axis(self.points, 2)
+
+        # Create a voxel grid representation of the map
+        num_voxels_per_axis = self.metres_to_voxel_coords([
+            extents_x[1] - extents_x[0],
+            extents_y[1] - extents_y[0],
+            extents_z[1] - extents_z[0],
+        ])
+        self.voxel_grid = np.zeros(num_voxels_per_axis)
+
+        # Fill in the voxel grid
+        for point in self.points:
+            x, y, z = point
+            i, j, k = self.metres_to_voxel_coords([x, y, z])
+            # If it's outside the grid, skip
+            if not self.voxel_coord_in_bounds([i, j, k]):
+                continue
+            else:
+                self.voxel_grid[i, j, k] = 1
+
+        print("Map loaded")
+        print(f"\t-map_filepath: {self.map_filepath}")
+        print(f"\t-voxel_per_x_metres: {self.voxel_per_x_metres}")
+        print(f"\t-num_points: {len(self.points)}")
+        print(f"\t-extents: x={extents_x}, y={extents_y}, z={extents_z}")
+        print(f"\t-voxel_grid (shape): {self.voxel_grid.shape}")
+
+    def voxel_coords_to_metres(
+        self,
+        voxel_coords,
+    ):
+        return np.array(voxel_coords) * self.voxel_per_x_metres
+    def metres_to_voxel_coords(
+        self,
+        metres_coords,
+    ):
+        _ = (np.array(metres_coords) / self.voxel_per_x_metres)
+        _ = np.floor(_).astype(int)
+        return _
+    def voxel_coord_in_bounds(
+        self,
+        voxel_coords,
+    ):
+        return all([0 <= x < self.voxel_grid.shape[i] for i, x in enumerate(voxel_coords)])
+
+    def plan_path(
+        self,
+        a_coord_metres,
+        b_coord_metres,
+        avoid_radius,
+    ):
+        """
+        Uses A* or RRT* or similar to compute a path from the start coordinate
+        to the finish coordinate, noting that the agent has a certain radius defining
+        a circle that cannot intersect with obstacles
+
+        Occupancy grid is matrix where 0 is free space and 1 is an obstacle,
+        and the scale is given by self.voxel_per_x_metres
+
+        Returns a list of coordinates in metres that define the points along the path
+        """
+
+        a_voxel_coord = (np.array(a_coord_metres) / self.voxel_per_x_metres).astype(int)
+        b_voxel_coord = (np.array(b_coord_metres) / self.voxel_per_x_metres).astype(int)
+
+        # TODO dilate grid to allow for avoidance
+
+        # Use A* algorithm for pathfinding
+        print(f"Making graph with shape {self.voxel_grid.shape}")
+        graph = nx.grid_graph(dim=self.voxel_grid.shape, periodic=False)
+        print(f"Graph has {len(graph.nodes)} nodes and {len(graph.edges)} edges")
+
+        # Define a custom heuristic function for A* (Euclidean distance)
+        def heuristic_euclidean(u, v):
+            (x1, y1, z1) = u
+            (x2, y2, z2) = v
+            return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+        # Define a custom weight function for A* (considering obstacle dilation)
+        def weight(u, v, data):
+            return data.get("weight", 1)
+
+        # Set weights for edges based on the dilated grid
+        edges = list(graph.edges(data=True))
+        with tqdm(total=len(edges), desc="Setting graph weights to encode obstacles", disable=True) as pbar:
+            for (u, v, data) in edges:
+                # u and v are both nodes, that are tuples of (x, y) coordinates, but they
+                # need to be transposed 
+                # u = (u[1], u[0])
+                # v = (v[1], v[0])
+                u = (u[2], u[1], u[0])
+                v = (v[2], v[1], v[0])
+                # TODO: > 0.5?
+                if self.voxel_grid[u] == 1 or self.voxel_grid[v] == 1:
+                    data["weight"] = float('inf')
+                pbar.update(1)
+
+        # Compute the path using A* algorithm
+        try:
+            path_coords = nx.astar_path(graph, a_voxel_coord, b_voxel_coord, heuristic=heuristic_euclidean, weight=weight)
+            # Convert path nodes back to coordinates in metres
+            path_metres = np.array(path_coords) * self.voxel_per_x_metres
+            
+            # Subsample so that we definitely have the start and final point, but otherwise
+            # only a point every X metres
+            # TODO
+
+            return path_metres
+        except nx.NetworkXNoPath:
+            raise ValueError(f"No path found between start ({a_coord_metres} m) and finish ({b_coord_metres} m) in the occupancy grid (shape: {self.voxel_grid.shape})")
