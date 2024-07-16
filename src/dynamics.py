23f27d5772fbafd372ae76d217546cf13592fda6
isaac-ward <isaacronaldward@gmail.com>
2024-07-16 14:10:56 -0700

    GPU agnostic batch step with CUDA acceleration implemented

--- src/dynamics.py
+++ src/dynamics.py
@@ -75,7 +75,7 @@ class DynamicsQuadcopter3D:
             [-magnitude_lo, +magnitude_hi],
             [-magnitude_lo, +magnitude_hi],
             [-magnitude_lo, +magnitude_hi],
-        ])#.get() 
+        ]) 
     
     def step(self, state, action):
         """
@@ -85,22 +85,63 @@ class DynamicsQuadcopter3D:
 
         state = np.asarray(state)
         action = np.asarray(action)
-
-        args = (state, action, self.diameter, self.mass, self.Ix, self.Iy, self.Iz, self.g, self.thrust_coef, self.drag_yaw_coef, self.drag_force_coef, self.dt)
         
-        # Check if we have a batch
-        batched = len(state.shape) == 2
-        if batched:
-            return step_batched(*args)
-        else:
-            return step_single(*args)
+        # If not batched then wrap in a batch
+        is_batch = len(state.shape) == 2
+        if not is_batch:
+            state = state[None, :]
+            action = action[None, :]
+
+        # If GPUs are available use them
+        if cp.cuda.is_available():
+            state = cp.asarray(state)
+            action = cp.asarray(action)
+
+        # Call step
+        new_states = step_batch_gpu(
+            state, action,
+            self.diameter, self.mass, self.Ix, self.Iy, self.Iz, self.g, 
+            self.thrust_coef, self.drag_yaw_coef, self.drag_force_coef, self.dt
+        )
+
+        # Unbatch if needed
+        if not is_batch:
+            new_states = new_states[0]
+
+        # If we used GPU compute, then move back to CPU
+        if cp.cuda.is_available():
+            new_states = new_states.get()
+        return new_states
+    
+    # def step(self, state, action):
+    #     """
+    #     This function works for both single state shaped (12,) and action shaped (4,)
+    #     and batched states shaped (B, 12) and batched actions shaped (B, 4)
+    #     """
+
+    #     state = np.asarray(state)
+    #     action = np.asarray(action)
+
+    #     args = (state, action, self.diameter, self.mass, self.Ix, self.Iy, self.Iz, self.g, self.thrust_coef, self.drag_yaw_coef, self.drag_force_coef, self.dt)
+        
+    #     # Check if we have a batch
+    #     batched = len(state.shape) == 2
+    #     if batched:
+    #         return step_batched(*args)
+    #     else:
+    #         return step_single(*args)
         
 # --------------------------------------------------------- 
 
-def step_single_gpu(state, action, diameter, mass, Ix, Iy, Iz, g, thrust_coef, drag_yaw_coef, drag_force_coef, dt):
+
+def step_batch_gpu(states, actions, diameter, mass, Ix, Iy, Iz, g, thrust_coef, drag_yaw_coef, drag_force_coef, dt):
     """
-    This function works for when we get a single state shaped (12,) and a single action shaped (4,).
+    This function works for batched states shaped (B, 12) and batched actions shaped (B, 4).
     """
+
+    # Do we have GPU access?
+    xp = cp.get_array_module(states)
+    
     # For convenience
     k = thrust_coef
     b = drag_yaw_coef
@@ -108,15 +149,18 @@ def step_single_gpu(state, action, diameter, mass, Ix, Iy, Iz, g, thrust_coef, d
 
     # Unwrap the state and action 
     # position, euler angles (xyz=>φθψ), velocity, body rates (eq2.23)
-    x, y, z, rz, ry, rx, xd, yd, zd, p, q, r = state
-    w1, w2, w3, w4 = action
-    # For convienience and to match with the KTH paper
+    x, y, z        = states[:, 0],  states[:, 1],  states[:, 2]
+    rz, ry, rx     = states[:, 3],  states[:, 4],  states[:, 5]
+    xd, yd, zd     = states[:, 6],  states[:, 7],  states[:, 8]
+    p, q, r        = states[:, 9],  states[:, 10], states[:, 11]
+    w1, w2, w3, w4 = actions[:, 0], actions[:, 1], actions[:, 2], actions[:, 3]
+    # For convenience and to match with the KTH paper
     ψ, θ, φ = rz, ry, rx
 
     # Compute sin, cos, and tan (xyz order is φ θ ψ)
-    s_ψ, c_ψ,     = cp.sin(ψ), cp.cos(ψ)
-    s_θ, c_θ, t_θ = cp.sin(θ), cp.cos(θ), cp.tan(θ)
-    s_φ, c_φ,     = cp.sin(φ), cp.cos(φ)
+    s_ψ, c_ψ      = xp.sin(ψ), xp.cos(ψ)
+    s_θ, c_θ, t_θ = xp.sin(θ), xp.cos(θ), xp.tan(θ)
+    s_φ, c_φ      = xp.sin(φ), xp.cos(φ)
 
     # Compute the control vector (control force, control torques), eq2.16
     w1_sq = w1 ** 2
@@ -131,31 +175,32 @@ def step_single_gpu(state, action, diameter, mass, Ix, Iy, Iz, g, thrust_coef, d
     tz = b * ((w2_sq + w4_sq) - (w1_sq + w3_sq))
 
     # Compute the change in state (eq 2.23, 2.24, 2.25)
-    state_delta = cp.zeros(12)
+    state_delta = xp.zeros_like(states)
 
     # Positions change according to velocity
-    state_delta[0] = xd
-    state_delta[1] = yd
-    state_delta[2] = zd
+    state_delta[:, 0] = xd
+    state_delta[:, 1] = yd
+    state_delta[:, 2] = zd
 
     # Euler angles change according to body rates
-    state_delta[3] = q * s_φ / c_θ + r * c_φ / c_θ
-    state_delta[4] = q * c_φ - r * s_φ
-    state_delta[5] = p + q * s_φ * t_θ + r * c_φ * t_θ
+    state_delta[:, 3] = q * s_φ / c_θ + r * c_φ / c_θ
+    state_delta[:, 4] = q * c_φ - r * s_φ
+    state_delta[:, 5] = p + q * s_φ * t_θ + r * c_φ * t_θ
 
     # Velocities change according to forces and moments
-    state_delta[6] =   - (ft / mass) * (s_ψ * s_φ  +  c_ψ * s_θ * c_φ)
-    state_delta[7] =   - (ft / mass) * (c_ψ * s_φ  -  s_ψ * s_θ * c_φ)
-    state_delta[8] = g - (ft / mass) * (c_θ * c_φ)
+    state_delta[:, 6] =   - (ft / mass) * (s_ψ * s_φ  +  c_ψ * s_θ * c_φ)
+    state_delta[:, 7] =   - (ft / mass) * (c_ψ * s_φ  -  s_ψ * s_θ * c_φ)
+    state_delta[:, 8] = g - (ft / mass) * (c_θ * c_φ)
 
     # Body rates change according to moments of inertia and torques
-    state_delta[9]  = ((Iy - Iz) * q * r + tx) / Ix
-    state_delta[10] = ((Iz - Ix) * p * r + ty) / Iy
-    state_delta[11] = ((Ix - Iy) * p * q + tz) / Iz
+    state_delta[:, 9]  = ((Iy - Iz) * q * r + tx) / Ix
+    state_delta[:, 10] = ((Iz - Ix) * p * r + ty) / Iy
+    state_delta[:, 11] = ((Ix - Iy) * p * q + tz) / Iz
     
     # Use the Euler method to compute the new state
-    state_new = state + dt * state_delta
-    return state_new
+    states_new = states + dt * state_delta
+    return states_new
+
 
 @njit
 def step_single(state, action, diameter, mass, Ix, Iy, Iz, g, thrust_coef, drag_yaw_coef, drag_force_coef, dt):
