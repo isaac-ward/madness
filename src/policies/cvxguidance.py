ac3354602dfca3a3a9cf1a650cce9c8fd770159c
riordk626 <riordk@stanford.edu>
2024-10-06 19:08:57 -0700

    added much funcitonality to convex class(es) but still need to flesh out SCP implementation and begin SDF constraints

--- src/policies/cvxguidance.py
+++ src/policies/cvxguidance.py
@@ -1,6 +1,8 @@
-import numpy as np
+# import numpy as np
 import cvxpy as cvx
 import cupy as cp
+import jax.numpy as jnp
+
 
 
 import utils.geometric
@@ -8,30 +10,48 @@ import utils.general
 import policies.costs
 import policies.samplers
 
+from src.dynamics_jax import DynamicsQuadcopter3D
+from src.standard import SDFSphere # CORRECT THIS
+from src.standard import Astar # CORRECT THIS
+
 # TODO
 # import sdf.sdf write and import sdf class that holds sphere locations and sizes (plus the field?)
 
-class ConvexSolver:
+class SCPSolver:
     def __init__(
             self,
-            dt,
             K,
-            dynamics,
-            state_history,
-            state_goal
+            dynamics: DynamicsQuadcopter3D,
+            state_goal,
+            trajInit = Astar,
+            horizon = 1
     ):
-        self.dynamics = dynamics
         self.K = K
-        self.dt = dt
-        self.action = cvx.Variable((self.K,2))
-        self.state = cvx.Variable(self.K + 1, self.dynamics.state_size())
+        self.dynamics = dynamics
+        self.state_goal = state_goal
+        
+        self.dt = dynamics.dt
+        self.horizon = horizon
+
+        self.nu = self.dynamics.action_size()
+        self.nx = self.dynamics.state_size()
+
+
+        self.action = cvx.Variable((self.K,self.nu))
+        self.state = cvx.Variable((self.K + 1, self.nx))
+        self.action.value, self.state.value = Astar()
         self.constraints = []
-        self.objective = cvx.sum( [ cvx.square(self.action[i,1]) + cvx.square(self.action[i,2]) for i in range(K) ] )
+        self.objective = cvx.sum( [ cvx.sum( [ cvx.square(self.action[k,u]) for u in range(self.nu) ] ) for k in range(self.K) ] )
+
+        self.step_count = 1
 
     def dyn_constraints(
-            self
+            self,
+            state_history,
+            action_history
     ):
-        self.constraints += []
+        A, B, C = self.dynamics.affinize(state_history[-1], action_history[-1])
+        self.constraints += [ self.state[k+1] == A@self.state[k] + B@self.action[k] + C for k in range(self.K) ]
 
     def sdf_constraints(
             self
@@ -39,18 +59,40 @@ class ConvexSolver:
         self.constraints += []
 
     def boundary_constraints(
-            self
+            self,
+            state_history,
     ):
-        self.constraints += []
+        self.constraints += [self.state[0] == state_history[-1]]
+        self.constraints += [self.state[self.K] == self.state_goal]
+        self.constraints += [self.state[k] <= self.dynamics.action_ranges[:,1] for k in range(self.K)]
+        self.constraints += [self.state[k] >= self.dynamics.action_ranges[:,0] for k in range(self.K)]
+
+    def update_constraints(
+            self,
+            state_history, 
+            action_history):
+        
+        self.dyn_constraints(state_history, action_history)
+        self.sdf_constraints()
+        self.boundary_constraints(state_history)
 
     def solve(
-            self
+            self,
+            state_history,
+            action_history
     ):
-        prob = cvx.Problem(cvx.Minimize(self.objective), self.constraints)
-        prob.solve()
+        if self.step_count%self.horizon == 0:
+            
+            self.update_constraints(state_history, action_history)
+            prob = cvx.Problem(cvx.Minimize(self.objective), self.constraints)
+            prob.solve()
+            
+            self.step_count = 1
 
-        optimal_action_history = self.action.value
-        optimal_state_history = self.state.value
+        optimal_action_history = self.action.value[self.step_count-1:]
+        optimal_state_history = self.state.value[self.step_count-1:]
+
+        self.step_count += 1
 
         return optimal_action_history, optimal_state_history
 
@@ -59,10 +101,12 @@ class PolicyConvex:
     def __init__(
             self,
             dynamics,
-            sdf_nearest
+            sdf_nearest,
+            K,
+            solverClass = SCPSolver,
             ):
         
-        self.solver = ConvexSolver
+        self.solver = solverClass(K, dynamics,)
         self.dynamics = dynamics
         
         self.sdf = sdf_nearest
@@ -94,8 +138,9 @@ class PolicyConvex:
 
     def act(
             self,
-            state_history
+            state_history,
+            action_history
     ):
     
-        optimal_action_history, optimal_state_history = self.solver.solve()
+        optimal_action_history, optimal_state_history = self.solver.solve(state_history, action_history)
         return optimal_action_history[0], optimal_state_history[0]
