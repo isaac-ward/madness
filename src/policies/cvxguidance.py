6f1601f5bb554ff53baabaab0f363ee90d8499a8
riordk626 <riordk@stanford.edu>
2024-10-10 14:42:23 -0700

    scp mostly in place; still need to finish sdf constraints; merging sdf soon

--- src/policies/cvxguidance.py
+++ src/policies/cvxguidance.py
@@ -22,68 +22,82 @@ class SCPSolver:
             self,
             K,
             dynamics: DynamicsQuadcopter3D,
-            state_goal,
-            trajInit = Astar,
-            horizon = 1
+            trajInit,
+            sdf:SDFSphere,
+            horizon = 1,
+            sig = 50
     ):
         self.K = K
         self.dynamics = dynamics
-        self.state_goal = state_goal
-        
+        # self.state_goal = state_goal
+
         self.dt = dynamics.dt
         self.horizon = horizon
+        self.sig = sig
 
         self.nu = self.dynamics.action_size()
         self.nx = self.dynamics.state_size()
 
-
         self.action = cvx.Variable((self.K,self.nu))
         self.state = cvx.Variable((self.K + 1, self.nx))
-        self.action.value, self.state.value = Astar()
+        self.slack = cvx.Variable((self.K*self.sdf.poly_count()))
+        # self.alpha = cvx.Variable(1)
+        self.action.value, self.state.value = trajInit
+        self.sdf = sdf
         self.constraints = []
-        self.objective = cvx.sum( [ cvx.sum( [ cvx.square(self.action[k,u]) for u in range(self.nu) ] ) for k in range(self.K) ] )
 
         self.step_count = 1
 
     def dyn_constraints(
             self,
-            state_history,
-            action_history
     ):
-        A, B, C = self.dynamics.affinize(state_history[-1], action_history[-1])
-        self.constraints += [ self.state[k+1] == A@self.state[k] + B@self.action[k] + C for k in range(self.K) ]
+        A, B, C = self.dynamics.affinize(self.state.value[:-1], self.action.value)
+        self.constraints += [ self.state[k+1] == A[k,:,:]@self.state[k] + B[k,:,:]@self.action[k] + C[k,:,:] for k in range(self.K) ]
 
     def sdf_constraints(
             self
     ):
+        cs,rs = self.sdf
+
+
         self.constraints += []
 
     def boundary_constraints(
             self,
+            state_goal,
             state_history,
     ):
         self.constraints += [self.state[0] == state_history[-1]]
-        self.constraints += [self.state[self.K] == self.state_goal]
+        self.constraints += [self.state[self.K] == state_goal]
         self.constraints += [self.state[k] <= self.dynamics.action_ranges[:,1] for k in range(self.K)]
         self.constraints += [self.state[k] >= self.dynamics.action_ranges[:,0] for k in range(self.K)]
 
     def update_constraints(
             self,
+            state_goal,
             state_history, 
             action_history):
         
         self.dyn_constraints(state_history, action_history)
         self.sdf_constraints()
-        self.boundary_constraints(state_history)
+        self.boundary_constraints(state_goal, state_history)
+
+    def update_objective(
+        self,
+        state_goal
+    ):
+        self.objective = cvx.sum( [ cvx.sum( [ cvx.square(self.action[k,u]) for u in range(self.nu) ] ) for k in range(self.K) ] ) + cvx.sum( [ cvx.square( cvx.norm(state_goal - self.state[k]) ) for k in range(self.K+1) ] )
 
     def solve(
             self,
+            state_goal,
             state_history,
             action_history
     ):
         if self.step_count%self.horizon == 0:
             
-            self.update_constraints(state_history, action_history)
+            self.update_constraints(state_goal, state_history, action_history)
+            self.update_objective(state_goal)
             prob = cvx.Problem(cvx.Minimize(self.objective), self.constraints)
             prob.solve()
             
@@ -106,7 +120,7 @@ class PolicyConvex:
             solverClass = SCPSolver,
             ):
         
-        self.solver = solverClass(K, dynamics,)
+        self.solver = solverClass(K, dynamics, )
         self.dynamics = dynamics
         
         self.sdf = sdf_nearest
@@ -144,3 +158,7 @@ class PolicyConvex:
     
         optimal_action_history, optimal_state_history = self.solver.solve(state_history, action_history)
         return optimal_action_history[0], optimal_state_history[0]
+    
+
+
+# -----------------------------------------------------------------------------------------------------------------------
