940e50f591f8ab03d8740c75f2c975ba4c2843a3
isaac-ward <isaacronaldward@gmail.com>
2025-01-24 22:49:46 +0000

    Updates to navigable space algorithm

--- src/standard.py
+++ src/standard.py
@@ -2,59 +2,29 @@ from dynamics import DynamicsQuadcopter3D
 from dynamics_linear import DynamicsQuadcopter3DLinear
 from mapping import Map
 
-def get_standard_dynamics_linear():
-    return DynamicsQuadcopter3DLinear(
-        diameter=0.2,
-        mass=2.5,
-        Ix=0.5,
-        Iy=0.5,
-        Iz=0.3,
+def _dynamics_arguments():
+    return {
+        "diameter": 0.5,
+        "mass": 2.5,
+        "Ix": 0.5,
+        "Iy": 0.5,
+        "Iz": 0.3,
         # +z is down
-        g=+9.81, 
+        "g": +9.81,
         # higher makes it easier to roll and pitch
-        thrust_coef=5,      
+        "thrust_coef": 5,
         # higher makes it easier to yaw
-        drag_yaw_coef=5,   
+        "drag_yaw_coef": 5,
         # higher values lower the max velocity
-        drag_force_coef=5,   
-        dt=0.025,
-    )
+        "drag_force_coef": 5,
+        "dt": 0.025,
+    }
+
+def get_standard_dynamics_linear():
+    return DynamicsQuadcopter3DLinear(**_dynamics_arguments())
 
 def get_standard_dynamics():
-    return DynamicsQuadcopter3D(
-        diameter=0.2,
-        mass=2.5,
-        Ix=0.5,
-        Iy=0.5,
-        Iz=0.3,
-        # +z is down
-        g=+9.81, 
-        # higher makes it easier to roll and pitch
-        thrust_coef=5,      
-        # higher makes it easier to yaw
-        drag_yaw_coef=5,   
-        # higher values lower the max velocity
-        drag_force_coef=5,   
-        dt=0.025,
-    )
-
-def get_standard_dynamics_quadcopter_3d():
-    return DynamicsQuadcopter3D(
-        diameter=0.2,
-        mass=2.5,
-        Ix=0.5,
-        Iy=0.5,
-        Iz=0.3,
-        # +z is down
-        g=+9.81, 
-        # higher makes it easier to roll and pitch
-        thrust_coef=5,      
-        # higher makes it easier to yaw
-        drag_yaw_coef=5,   
-        # higher values lower the max velocity
-        drag_force_coef=5,   
-        dt=0.025,
-    )
+    return DynamicsQuadcopter3D(**_dynamics_arguments())
 
 def get_standard_map():
     return Map(
@@ -70,7 +40,7 @@ def get_standard_map():
 def get_28x28x28_at_111():
     return Map(
         map_filepath="/workspace/assets/maps/28x28x28_at_1-1-1.obj",
-        voxel_per_x_metres=0.25,
+        voxel_per_x_metres=1,
         extents_metres_xyz=[
             [0, 30], 
             [0, 30], 
@@ -81,7 +51,7 @@ def get_28x28x28_at_111():
 def get_28x28x28_at_111_with_obstacles():
     return Map(
         map_filepath="/workspace/assets/maps/28x28x28_at_1-1-1_with_obstacles.obj",
-        voxel_per_x_metres=0.25,
+        voxel_per_x_metres=1, # Any lower and point cloud is to sparse for connectivity
         extents_metres_xyz=[
             [0, 30], 
             [0, 30], 
@@ -90,7 +60,7 @@ def get_28x28x28_at_111_with_obstacles():
     )
 
 def get_standard_flow_action_dist_policy_arguments():
-    dyn = get_standard_dynamics_jax_quadcopter_3d()
+    dyn = get_standard_dynamics()
     map_ = get_standard_map()
     return {
         "dynamics": dyn,
