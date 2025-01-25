17053d6199682e648336f294a2b9e41d7429b0e6
isaac.ward <isaacronaldward@gmail.com>
2025-01-25 00:40:28 +0000

    Relaxed goal constraints

--- src/environment.py
+++ src/environment.py
@@ -33,8 +33,8 @@ class Environment:
         # If we're this close to an obstacle or the goal, we're done
         self.close_enough_position = self.dynamics.diameter / 2
         self.close_enough_orientation = 0.2 # radians
-        self.close_enough_velocity = 0.1 # m/s
-        self.close_enough_angular_velocity = 0.1 # rad/s
+        self.close_enough_velocity = 5 # m/s
+        self.close_enough_angular_velocity = 10000 # rad/s
 
         # Keep track of the history of states and actions
         self.state_history_tracker  = ItemHistoryTracker(item_shape=(self.dynamics.state_size(),))
